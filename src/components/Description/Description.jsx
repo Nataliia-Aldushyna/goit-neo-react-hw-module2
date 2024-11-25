a003d92b176c523e12ea3f495efb96497f2b4b4f
Nataliia Aldushyna <nataliia.aldushyna@gmail.com>
2024-11-25 21:43:32 +0100

    update description

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,9 @@
+import styles from "./Description.module.css";
+
+const Description = () => (
+  <p className={styles.description}>
+    Please leave your feedback about our service by selecting one of the options below.
+  </p>
+);
+
+export default Description;
