4afa799cd0e91ad368a285ff30f0f005b8a0ef34
Nataliia Aldushyna <nataliia.aldushyna@gmail.com>
2024-11-25 21:04:47 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import { useEffect, useState } from "react";
+import Notification from "./components/Notification/Notification";
+import {
+  getInitialFeedback,
+  calcFeedbackTotal,
+  calcFeedbackPositive,
+} from "./helpers/feedback";
+import { defaultFeedback, FEEDBACK_KEY } from "../feedback.config";
+import Container from "./components/Container/Container";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [feedback, setFeedback] = useState(getInitialFeedback);
+  const totalFeedback = calcFeedbackTotal(feedback);
+  const positiveFeedback = calcFeedbackPositive(feedback);
+
+  const updateFeedback = (feedbackType) => {
+    if (feedbackType === "reset") {
+      setFeedback({
+        ...defaultFeedback,
+      });
+      return;
+    }
+    setFeedback({
+      ...feedback,
+      [feedbackType]: feedback[feedbackType] + 1,
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem(FEEDBACK_KEY, JSON.stringify(feedback));
+  }, [feedback]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container>
+      <Description />
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} />
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} positiveFeedback={positiveFeedback} />
+      ) : (
+        <Notification />
+      )}
+    </Container>
+  );
 }
 
-export default App
+export default App;
