c260d74d70b2fa3514a508c7df87d1705fbf0f7e
Nataliia Aldushyna <nataliia.aldushyna@gmail.com>
2024-11-29 21:59:29 +0100

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,42 @@
 import { useState, useEffect } from "react";
-import Description from "./components/Description/Description";
-import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
-import styles from "./App.module.css";
 
 const App = () => {
   const [feedback, setFeedback] = useState(() => {
-    const savedFeedback = JSON.parse(localStorage.getItem("feedback"));
-    return savedFeedback || { good: 0, neutral: 0, bad: 0 };
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback ? JSON.parse(savedFeedback) : { good: 0, neutral: 0, bad: 0 };
   });
-
-  const updateFeedback = (feedbackType) => {
-    setFeedback((prev) => ({
-      ...prev,
-      [feedbackType]: prev[feedbackType] + 1,
-    }));
-  };
-
-  const resetFeedback = () => {
-    setFeedback({ good: 0, neutral: 0, bad: 0 });
-  };
-
-  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-  const positivePercentage = totalFeedback
-    ? Math.round((feedback.good / totalFeedback) * 100)
-    : 0;
 
   useEffect(() => {
     localStorage.setItem("feedback", JSON.stringify(feedback));
   }, [feedback]);
 
+  const onLeaveFeedback = (option) => {
+    setFeedback((prevState) => ({
+      ...prevState,
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const positivePercentage = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
   return (
-    <div className={styles.app}>
-      <h1>Sip Happens Café</h1>
-      <Description />
-      <Options
-        updateFeedback={updateFeedback}
-        resetFeedback={resetFeedback}
-        totalFeedback={totalFeedback}
-      />
+    <div>
+      <h1>Feedback App</h1>
       {totalFeedback > 0 ? (
         <Feedback
-          good={feedback.good}
-          neutral={feedback.neutral}
-          bad={feedback.bad}
+          feedback={feedback}
           total={totalFeedback}
-          positive={positivePercentage}
+          positivePercentage={positivePercentage}
+          onLeaveFeedback={onLeaveFeedback}
         />
       ) : (
-        <Notification message="No feedback yet!" />
+        <Notification message="No feedback given" />
       )}
     </div>
   );
